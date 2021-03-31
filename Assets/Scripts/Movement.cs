d7e6bc698148319d3499d68323414ed47a8fab49
Peally <49279978+Peally@users.noreply.github.com>
2021-03-31 08:25:57 +0100

    Test to try to fix after prefab disaster

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,17 +7,11 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] private float engineThrust = 1000f;
     [SerializeField] private float rotationSpeed = 100f;
-    [SerializeField] AudioClip mainEngine;
-    
     Rigidbody rb;
-    AudioSource rocketAudioSource;
-
-
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rocketAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -41,7 +35,7 @@ public class Movement : MonoBehaviour
 
     private void ApplyRotation(float rotationThisFrame)
     {
-        rb.freezeRotation = true; //Freezes the physics based rotation for rigidbody (for collisions etc)
+        rb.freezeRotation = true; //Freexing the physics based rotation for rigidbody (for collisions etc)
         transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
         rb.freezeRotation = false; //Unfreezing rotation so that physics engine can take over
     }
@@ -50,15 +44,9 @@ public class Movement : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.Space))
         {
-            if(!rocketAudioSource.isPlaying)
-            {
-                rocketAudioSource.PlayOneShot(mainEngine);
-            }
+ 
             rb.AddRelativeForce(Vector3.up * engineThrust * Time.deltaTime);
-        }
-        else
-        {
-            rocketAudioSource.Stop();
+
         }
 
     }
